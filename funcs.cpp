9213ad3ccf4ddbea23b868b20aa24fbb6a04de0d
Leontovich Nikita <varygstavr@yandex.ru>
2019-11-22 15:48:56 +0300

    add function MyDiv()

--- funcs.cpp
+++ funcs.cpp
@@ -3,10 +3,17 @@
 #include "funcs.h"   
 double MySum(double a, double b) {  
 return a + b; 
-}  
+} 
+
+ 
 double MySub(double a, double b) {
   return a - b; 
 }  
 double MyMul(double a, double b) {
   return a * b; 
 }
+
+double MyDiv(double a, double b) 
+{  
+return a / b; 
+} 
